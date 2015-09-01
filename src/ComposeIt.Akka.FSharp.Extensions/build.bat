c168ab320a7aa4c5d06c5e40a64874fef7b3b847
Thomas Jaskula <thomasv1000@hotmail.fr>
2015-09-01 14:08:57 +0200

    Add build.bat for Nuget command line FAKE installation

---
+++ src/ComposeIt.Akka.FSharp.Extensions/build.bat
@@ -0,0 +1,7 @@
+﻿
+@echo off
+cls
+"..\packages\NuGet.CommandLine.2.8.6\tools\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "..\..\packages" "-ExcludeVersion"
+"..\packages\NuGet.CommandLine.2.8.6\tools\NuGet.exe" "Install" "FAKE.SQL" "-OutputDirectory" "..\..\packages" "-ExcludeVersion"
+"..\packages\NuGet.CommandLine.2.8.6\tools\NuGet.exe" "Install" "FAKE.SQL.x64" "-OutputDirectory" "..\..\packages" "-ExcludeVersion"
+pause
