790950f6ed2fd26191c5e550e69f91003b6cd08d
Thomas Jaskula <thomasv1000@hotmail.fr>
2015-09-04 11:30:32 +0200

    Delete empty space in the build.bat

--- src/ComposeIt.Akka.FSharp.Extensions/build.bat
+++ src/ComposeIt.Akka.FSharp.Extensions/build.bat
@@ -1,5 +1,4 @@
-﻿
-@echo off
+﻿@echo off
 cls
 "..\packages\NuGet.CommandLine.2.8.6\tools\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "..\..\packages" "-ExcludeVersion"
 "..\..\packages\FAKE\tools\Fake.exe" build.fsx
