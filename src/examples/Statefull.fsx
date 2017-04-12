8d7f44c9da04a3e4a77271792ca092c2fdf5da20
Jaskula Tomasz <thomasv1000@hotmail.fr>
2017-04-13 00:09:06 +0200

    Refactoring statefull example

--- src/examples/Statefull.fsx
+++ src/examples/Statefull.fsx
@@ -1,33 +1,26 @@
-﻿// sets the current directory to be same as the script directory
-System.IO.Directory.SetCurrentDirectory (__SOURCE_DIRECTORY__)
-
-#r @"../ComposeIt.Akka.FSharp.Extensions/bin/Debug/Akka.dll"
+﻿#r @"../ComposeIt.Akka.FSharp.Extensions/bin/Debug/Akka.dll"
 #r @"../ComposeIt.Akka.FSharp.Extensions/bin/Debug/Akka.FSharp.dll"
 #r @"../ComposeIt.Akka.FSharp.Extensions/bin/Debug/ComposeIt.Akka.FSharp.Extensions.dll"
 
 open Akka.FSharp
 open ComposeIt.Akka.FSharp.Extensions.Lifecycle
 
-let preStartCalled = ref false
-let preStart = Some(fun (baseFn : unit -> unit) -> preStartCalled := true)
-
 type Message =
-    | Hi
-    | Greet of string
+    | Print
+    | MyName of string
 
-let rec greeter lastKnown = function
-    | Hi -> printfn "Who sent Hi? %s?" lastKnown |> empty
-    | Greet(who) ->
-        printfn "%s sends greetings" who
-        become (greeter who)
+let rec namePrinter lastName = function
+    | Print -> printfn "Last name was %s?" lastName |> empty
+    | MyName(who) ->
+        printfn "Hello %s!" who
+        become (namePrinter who)
 
 let system = System.create "testSystem" (Configuration.load())
 
 let actor = 
-    spawnOvrd system "actor" 
-    <| actorOf (greeter "Me")
-    <| {defOvrd with PreStart = preStart}
+    spawn system "actor" 
+    <| actorOf (namePrinter "No One")
 
-actor <! Greet "Tom"
-actor <! Greet "Jane"
-actor <! Hi
+actor <! MyName "Tomasz"
+actor <! MyName "Marcel"
+actor <! Print
