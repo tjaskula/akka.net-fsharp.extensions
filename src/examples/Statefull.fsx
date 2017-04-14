8188d6de3c5d8139e7b3df668723bfabe7c7afc1
Jaskula Tomasz <thomasv1000@hotmail.fr>
2017-04-14 13:22:39 +0200

    Add another statefull sample

--- src/examples/Statefull.fsx
+++ src/examples/Statefull.fsx
@@ -5,6 +5,9 @@
 open Akka.FSharp
 open ComposeIt.Akka.FSharp.Extensions.Lifecycle
 
+let system = System.create "testSystem" (Configuration.load())
+
+// 1. Basic sample
 type Message =
     | Print
     | MyName of string
@@ -15,8 +18,6 @@ let rec namePrinter lastName = function
         printfn "Hello %s!" who
         become (namePrinter who)
 
-let system = System.create "testSystem" (Configuration.load())
-
 let actor = 
     spawn system "actor" 
     <| actorOf (namePrinter "No One")
@@ -24,3 +25,38 @@ let actor =
 actor <! MyName "Tomasz"
 actor <! MyName "Marcel"
 actor <! Print
+
+
+// 2. More advanced sample
+type Message' =
+    | PlayMovie of string
+    | StopMovie
+
+type ActorState =
+    | Playing of string
+    | Stopped of string
+
+let rec moviePlayer lastState = function
+    | PlayMovie m -> 
+        match lastState with
+        | Playing _ -> printfn "Error: cannot start playing another movie before stopping existing one"
+        | Stopped t -> printfn "Currently watching %s" t
+                       printfn "User Actor has now become Playing"
+        become (moviePlayer (Playing m))        
+    | StopMovie -> 
+        match lastState with
+        | Playing t -> printfn "Stopped watching %s" t
+                       printfn "User Actor has now become Stopped"
+        | Stopped _ -> printfn "Error: cannot stop if nothing is playing"
+        become (moviePlayer (Stopped ""))                    
+    
+let aref = 
+    spawn system "UserActorBecome"
+    <| actorOf( printfn "Creating a UserActor"
+                printfn "Setting initial behavior to Stopped"
+                moviePlayer (Stopped ""))
+
+aref <! PlayMovie "The Walking Dead"
+aref <! PlayMovie "Breaking Bad"
+aref <! StopMovie
+aref <! StopMovie
