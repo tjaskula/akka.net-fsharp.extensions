032e8d90a2f9c0dd90d31205104562e4726c3a4e
Jaskula Tomasz <thomasv1000@hotmail.fr>
2017-04-19 16:13:16 +0200

    Updating Path in build scripts

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 ﻿@echo off
 cls
-".\packages\NuGet.CommandLine.3.4.3\tools\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "..\packages"
+".\packages\NuGet.CommandLine\tools\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages/"
 ".\packages\FAKE\tools\Fake.exe" build.fsx
 pause
