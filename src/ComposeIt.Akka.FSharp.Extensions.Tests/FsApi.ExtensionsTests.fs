def020e17dee23b888410ce82ee6d35cb6336d50
Jaskula Tomasz <thomasv1000@hotmail.fr>
2017-05-03 09:41:01 +0200

    adding new UT for exception handling

--- src/ComposeIt.Akka.FSharp.Extensions.Tests/FsApi.ExtensionsTests.fs
+++ src/ComposeIt.Akka.FSharp.Extensions.Tests/FsApi.ExtensionsTests.fs
@@ -4,7 +4,6 @@ open Akka.FSharp
 open Akka.Actor
 open ComposeIt.Akka.FSharp.Extensions.Actor
 open System
-open System.Threading
 open System.Threading.Tasks
 open Xunit
 
@@ -75,85 +74,122 @@ let ``can override PreRestart methods when starting actor with computation expre
         <| fun mailbox ->
             let rec loop() = actor {
                 let! msg = mailbox.Receive()
+                let tp = typeof<string>
                 match msg with
                 | Lifecycle e -> 
                     match e with
-                    | PreRestart(_, _) -> preRestartCalled := true
+                    | PreRestart(_) -> preRestartCalled := true
                     | _ -> ()
-                | Message m -> 
+                | Message m ->
                     if m = "restart"
                     then failwith "System must be restarted"
                     else mailbox.Sender() <! m
-                | _ -> mailbox.Sender() <! msg
+                | t -> mailbox.Sender() <! t
                 return! loop ()
             }
             loop ()
-        <| [ SpawnOption.SupervisorStrategy (Strategy.OneForOne (fun error ->
-                Directive.Restart)) ]
+        <| [ SpawnOption.SupervisorStrategy (Strategy.OneForOne (fun _ -> Directive.Restart)) ]
     actor <! "restart"
-    let response = actor <? "msg" |> Async.RunSynchronously
+    actor <? "msg" |> Async.RunSynchronously
     system.Terminate() |> ignore
     system.WhenTerminated.Wait(TimeSpan.FromSeconds(2.)) |> ignore
     !preRestartCalled |> equals true
 
-[<Fact>]
-let ``can override PostRestart methods when starting actor with computation expression`` () =
-    
-    let postRestartCalled = ref false
-    
-    use system = System.create "testSystem" (Configuration.load())
-    let actor = 
-        spawnOpt system "actor" 
-        <| fun mailbox ->
-            let rec loop() = actor {
-                let! msg = mailbox.Receive()
-                match msg with
-                | Lifecycle e -> 
-                    match e with
-                    | PostRestart exn -> postRestartCalled := true
-                    | _ -> ()
-                | Message m -> 
-                    if m = "restart"
-                    then failwith "System must be restarted"
-                    else mailbox.Sender() <! m
-                | _ -> mailbox.Sender() <! msg
-                return! loop ()
-            }
-            loop ()
-        <| [ SpawnOption.SupervisorStrategy (Strategy.OneForOne (fun error ->
-                Directive.Restart)) ]
-    actor <! "restart"
-    let response = actor <? "msg" |> Async.RunSynchronously
-    system.Terminate() |> ignore
-    system.WhenTerminated.Wait(TimeSpan.FromSeconds(2.)) |> ignore
-    !postRestartCalled |> equals true
-
-type Message =
-    | Print
-    | MyName of string
-
-[<Fact>]
-let ``can change behaviour with become`` () =
-    
-    let answer = ref "no one"
-
-    let rec namePrinter lastName = function
-        | Message m ->
-        match m with
-            | Print -> answer := sprintf "Last name was %s?" lastName 
-                       empty
-            | MyName(who) ->
-                become (namePrinter who)
-        | _ -> become (namePrinter lastName)
-
-    use system = System.create "testSystem" (Configuration.load())
-    let actor = 
-        spawn system "actor" 
-        <| actorOf (namePrinter "No one")
-    actor <! MyName "Tomasz"
-    actor <! MyName "Marcel"
-    actor <! Print
-    Task.Delay(100).Wait()
-    system.Terminate() |> ignore
-    system.WhenTerminated.Wait(TimeSpan.FromSeconds(2.)) |> ignore
-    (answer.Value) |> equals ("Last name was Marcel?")
+//[<Fact>]
+//let ``can override PostRestart methods when starting actor with computation expression`` () =
+//    
+//    let postRestartCalled = ref false
+//    
+//    use system = System.create "testSystem" (Configuration.load())
+//    let actor = 
+//        spawnOpt system "actor" 
+//        <| fun mailbox ->
+//            let rec loop() = actor {
+//                let! msg = mailbox.Receive()
+//                match msg with
+//                | Lifecycle e -> 
+//                    match e with
+//                    | PostRestart exn -> postRestartCalled := true
+//                    | _ -> ()
+//                | Message m -> 
+//                    if m = "restart"
+//                    then failwith "System must be restarted"
+//                    else mailbox.Sender() <! m
+//                | _ -> mailbox.Sender() <! msg
+//                return! loop ()
+//            }
+//            loop ()
+//        <| [ SpawnOption.SupervisorStrategy (Strategy.OneForOne (fun error ->
+//                Directive.Restart)) ]
+//    actor <! "restart"
+//    actor <? "msg" |> Async.RunSynchronously
+//    system.Terminate() |> ignore
+//    system.WhenTerminated.Wait(TimeSpan.FromSeconds(2.)) |> ignore
+//    !postRestartCalled |> equals true
+//
+//type Message =
+//    | Print
+//    | MyName of string
+//
+//[<Fact>]
+//let ``can change behaviour with become`` () =
+//    
+//    let answer = ref "no one"
+//
+//    let rec namePrinter lastName = function
+//        | Message m ->
+//        match m with
+//            | Print -> answer := sprintf "Last name was %s?" lastName 
+//                       empty
+//            | MyName(who) ->
+//                become (namePrinter who)
+//        | _ -> become (namePrinter lastName)
+//
+//    use system = System.create "testSystem" (Configuration.load())
+//    let actor = 
+//        spawn system "actor" 
+//        <| actorOf (namePrinter "No one")
+//    actor <! MyName "Tomasz"
+//    actor <! MyName "Marcel"
+//    actor <! Print
+//    Task.Delay(100).Wait()
+//    system.Terminate() |> ignore
+//    system.WhenTerminated.Wait(TimeSpan.FromSeconds(2.)) |> ignore
+//    (answer.Value) |> equals ("Last name was Marcel?")
+//
+//type PlayMovieMessage = {MovieTitle : string; UserId : int}
+//
+//type PlayerMessage =
+//    | PlayMovie of PlayMovieMessage
+//    | StopMovie
+//
+//[<Fact>]
+//let ``should forward not expected messages types to unhandled mailbox`` () =
+//    
+//    let answer = ref (Message(0))
+//
+//    use system = System.create "testSystem" (Configuration.load())
+//    let actor = 
+//        spawn system "actor"
+//        <| fun mailbox ->
+//            let rec loop() = actor {                
+//                let! msg = mailbox.Receive()
+//                match msg with
+//                    | Lifecycle _ -> ()
+//                    | Message m -> 
+//                        match m with
+//                        | msg when msg.UserId > 40 -> ()
+//                        | t -> answer := msg
+//                               mailbox.Unhandled msg
+//
+//                return! loop()
+//            }
+//            loop()
+//    actor <! {MovieTitle = "Akka.NET : The Movie"; UserId = 42}
+//    actor <! {MovieTitle = "Partial Recall"; UserId = 99}
+//    actor <! {MovieTitle = "Boolean Lies"; UserId = 77}
+//    actor <! {MovieTitle = "Codenan the Destroyer"; UserId = 1}
+//    actor <! 87
+//    system.Terminate() |> ignore
+//    system.WhenTerminated.Wait(TimeSpan.FromSeconds(2.)) |> ignore
+//    answer.Value |> equals (Message(87))
