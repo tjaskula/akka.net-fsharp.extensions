7342eb1613ce4cf6d6261f6f49ab5c619707caa3
Jaskula Tomasz <thomasv1000@hotmail.fr>
2017-07-20 22:50:29 +0200

    rename build.bat to build.cmd. Add conditional checks for installing FAKE and excluding version

--- build.bat
+++
@@ -1,5 +0,0 @@
-﻿@echo off
-cls
-".\packages\NuGet.CommandLine\tools\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages/"
-".\packages\FAKE.4.61.0\tools\Fake.exe" build.fsx
-pause
---
+++ build.cmd
@@ -0,0 +1,12 @@
+﻿@echo off
+
+IF NOT EXIST packages\FAKE\tools\FAKE.exe  (
+  .\packages\NuGet.CommandLine\tools\NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+)
+
+IF NOT EXIST build.fsx (
+  packages\FAKE\tools\FAKE.exe init.fsx
+)
+packages\FAKE\tools\FAKE.exe build.fsx %*
+
+pause
