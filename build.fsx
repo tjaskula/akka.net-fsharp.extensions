91c3c48c09ca75b1d505e7464169ee2c19a9c323
Tomasz Jaskula <thomasv1000@hotmail.fr>
2018-03-17 20:27:12 +0100

    Moving the whole project to .netstandard

--- build.fsx
+++ build.fsx
@@ -1,88 +1,168 @@
-﻿// sets the current directory to be same as the script directory
-System.IO.Directory.SetCurrentDirectory (__SOURCE_DIRECTORY__)
+﻿// --------------------------------------------------------------------------------------
+// FAKE build script
+// --------------------------------------------------------------------------------------
 
-#r @"./packages/FAKE/tools/FakeLib.dll"
-#r "System.Configuration.dll"
-
+#r @"packages/FAKE/tools/FakeLib.dll"
 open Fake
-
+open Fake.Git
+open Fake.AssemblyInfoFile
+open Fake.ReleaseNotesHelper
 open System
+open System.IO
 
-let authors = ["Tomasz Jaskula (@tjaskula)"]
+// --------------------------------------------------------------------------------------
+// START TODO: Provide project-specific details below
+// --------------------------------------------------------------------------------------
 
-// project name and description
-let projectName = "Akka.NET.FSharp.API.Extensions"
-let projectDescription = "Set of extensions to the Akka.NET F# API that are not available in the standard library. This includes actor lifecycle managment and stateful actors."
-let projectSummary = "Set of extensions to the official Akka.NET F# API."
+// Information about the project are used
+//  - for version and project name in generated AssemblyInfo file
+//  - by the generated NuGet package
+//  - to run tests and to publish documentation on GitHub gh-pages
+//  - for documentation, you also need to edit info in "docs/tools/generate.fsx"
 
-// directories
-let buildDir = "./src/Akka.FSharp.Extensions/bin"
-let packagingRoot = "./packaging/"
-let packagingDir = packagingRoot @@ "AkkaFSharpExtensions"
+// The name of the project
+// (used by attributes in AssemblyInfo, name of a NuGet package and directory in 'src')
+let project = "Akka.FSharp.Extensions"
 
-let buildMode = getBuildParamOrDefault "buildMode" "Release"
+// Short summary of the project
+// (used as description in AssemblyInfo and as a short summary for NuGet package)
+let summary = "Set of extensions to the official Akka.NET F# API."
 
-MSBuildDefaults <- { 
-    MSBuildDefaults with 
-        ToolsVersion = Some "15.0"
-        Verbosity = Some MSBuildVerbosity.Minimal }
+// Longer description of the project
+// (used as a description for NuGet package; line breaks are automatically cleaned up)
+let description = "Set of extensions to the Akka.NET F# API that are not available in the standard library. This includes actor lifecycle managment and stateful actors."
+
+// List of author names (for NuGet package)
+let authors = [ "Tomasz Jaskula" ]
+
+// Tags for your project (for NuGet package)
+let tags = "akka.net fsharp"
+
+// File system information 
+let solutionFile  = "FSharp.Extensions.sln"
+
+// Pattern specifying assemblies to be tested using NUnit
+let testAssemblies = "tests/Akka.FSharp.Extensions.Tests/bin/Release/**/*Tests.dll"
+
+// Git configuration (used for publishing documentation in gh-pages branch)
+// The profile where the project is posted
+let gitOwner = "tjaskula" 
+let gitHome = "https://github.com/" + gitOwner
+
+// The name of the project on GitHub
+let gitName = "akka.net-fsharp.extensions"
+
+// The url for the raw files hosted
+let gitRaw = environVarOrDefault "gitRaw" "https://raw.github.com/tjaskula"
+
+// --------------------------------------------------------------------------------------
+// END TODO: The rest of the file includes standard build steps
+// --------------------------------------------------------------------------------------
+
+// Read additional information from the release notes document
+let release = LoadReleaseNotes "RELEASE_NOTES.md"
+
+// Generate assembly info files with the right version & up-to-date information
+Target "AssemblyInfo" <| fun _ ->
+    let setProjectDetails project = 
+        XmlPokeInnerText project "//Project/PropertyGroup/Version" release.AssemblyVersion    
+        XmlPokeInnerText project "//Project/PropertyGroup/PackageReleaseNotes" (release.Notes |> String.concat "\n")
+
+    !! "src/**/*.fsproj" |> Seq.iter setProjectDetails
+
+// Copies binaries from default VS location to expected bin folder
+// But keeps a subdirectory structure for each project in the 
+// src folder to support multiple project outputs
+Target "CopyBinaries" (fun _ ->
+    !! "src/**/*.??proj"
+    |>  Seq.map (fun f -> ((System.IO.Path.GetDirectoryName f) @@ "bin/Release", "bin" @@ (System.IO.Path.GetFileNameWithoutExtension f)))
+    |>  Seq.iter (fun (fromDir, toDir) -> CopyDir toDir fromDir (fun _ -> true))
+)
+
+// --------------------------------------------------------------------------------------
+// Clean build results
 
 Target "Clean" (fun _ ->
-    CleanDirs [buildDir; packagingRoot; packagingDir]
+    CleanDirs ["bin"; "temp"]
 )
 
-let setParams defaults = {
-    defaults with
-        ToolsVersion = Some("15.0")
-        Targets = ["Build"]
-        Properties =
-            [
-                "Configuration", buildMode
-            ]
-    }
+// --------------------------------------------------------------------------------------
+// Build library & test project
 
-Target "BuildApp" (fun _ ->
-    build setParams "./src/FSharp.Extensions.sln"
-        |> DoNothing
+Target "Build" (fun _ ->
+    !! solutionFile
+    |> MSBuildRelease "" "Rebuild"
+    |> ignore
 )
 
-Target "CreatePackage" (fun _ ->
-
-    let net45Dir = packagingDir @@ "lib/net45/"
-
-    CleanDirs [net45Dir]
-
-    CopyFile net45Dir (buildDir @@ "Release/Akka.FSharp.Extensions.dll")
-    CopyFile net45Dir (buildDir @@ "Release/Akka.FSharp.Extensions.XML")
-    CopyFile net45Dir (buildDir @@ "Release/Akka.FSharp.Extensions.pdb")
-
-    NuGet (fun p -> 
-        {p with
-            Authors = authors
-            Project = projectName
-            Description = projectDescription                               
-            OutputPath = packagingRoot
-            Summary = projectSummary
-            WorkingDir = packagingDir
-            Version = "0.2.5.0"
-            Dependencies =
-                ["Akka", GetPackageVersion "./packages/" "Akka"
-                 "Akka.FSharp", GetPackageVersion "./packages/" "Akka.FSharp"
-                 "FsPickler", GetPackageVersion "./packages/" "FsPickler"
-                 "FSharp.Quotations.Evaluator", GetPackageVersion "./packages/" "FSharp.Quotations.Evaluator"
-                 "Newtonsoft.Json", GetPackageVersion "./packages/" "Newtonsoft.Json"]
-            Files = [
-                     (@"lib\**\*.*", Some "lib", None)
-                     (@"..\..\Install.ps1", Some "tools", None)]
-            AccessKey = getBuildParamOrDefault "nugetkey" ""
-            Publish = hasBuildParam "nugetkey" }) 
-            "Akka.FSharp.Extensions.nuspec"
+// --------------------------------------------------------------------------------------
+// Run the unit tests using test runner
+open Fake.Testing.XUnit2
+Target "RunTests" (fun _ ->
+    !! testAssemblies
+    |> xUnit2 (fun p ->
+        { p with
+            TimeOut = TimeSpan.FromMinutes 20.
+            XmlOutputPath = Some "TestResults.xml"
+            ToolPath = "packages/xunit.runner.console/tools/net452/xunit.console.exe" })
 )
+
+// --------------------------------------------------------------------------------------
+// Build a NuGet package
+
+Target "NuGet" <| fun _ ->
+    let pack project = 
+        DotNetCli.Pack(fun p -> 
+            { p with
+                Project = project
+                OutputPath = "bin"
+                Configuration = "Release"
+                AdditionalArgs = ["--include-symbols"] })
+    
+    !! "src/**/*.fsproj" |> Seq.iter pack
+
+
+Target "PublishNuget" (fun _ ->
+    Paket.Push(fun p -> 
+        { p with
+            WorkingDir = "bin"
+            DegreeOfParallelism = 1 })
+)
+
+#load "paket-files/fsharp/FAKE/modules/Octokit/Octokit.fsx"
+open Octokit
+
+Target "Release" (fun _ ->
+    StageAll ""
+    Git.Commit.Commit "" (sprintf "Bump version to %s" release.NugetVersion)
+    Branches.push ""
+
+    Branches.tag "" release.NugetVersion
+    Branches.pushTag "" "origin" release.NugetVersion
+    
+    // release on github
+    createClient (getBuildParamOrDefault "github-user" "") (getBuildParamOrDefault "github-pw" "")
+    |> createDraft gitOwner gitName release.NugetVersion (release.SemVer.PreRelease <> None) release.Notes 
+    // TODO: |> uploadFile "PATH_TO_FILE"    
+    |> releaseDraft
+    |> Async.RunSynchronously
+)
+
+Target "BuildPackage" DoNothing
+
+// --------------------------------------------------------------------------------------
+// Run all targets by default. Invoke 'build <Target>' to override
+
+Target "All" DoNothing
 
 "Clean"
-   ==> "BuildApp"
+  ==> "AssemblyInfo"
+  ==> "Build"
+  ==> "CopyBinaries"
+  ==> "All"
 
-"BuildApp"
-   ==> "CreatePackage"
+"All" 
+  ==> "NuGet"
+  ==> "BuildPackage"
 
-RunTargetOrDefault "CreatePackage"
+RunTargetOrDefault "All"
